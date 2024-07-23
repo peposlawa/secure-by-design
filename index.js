cc11dc361866b19f5da211a741cbd8adb0c33e43
kieranp-p6 <kiip96@icloud.com>
2024-07-23 12:27:32 +0100

    completed user stroy 2, The helmet is encrypting

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import helmet from "helmet";
 
 const app = express();
+app.use(helmet());
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send({'message': 'Hello World!'})
+    res.status(200).send({ 'message': 'Hello World!' })
     // res.status(Status code:200).send("Hello world!")
 })
 app.listen(port, () => {
