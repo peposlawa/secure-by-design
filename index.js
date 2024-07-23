9fb5b82ff149634dbde7eab80085a509ab430cda
peposlawa <peposlawa@gmail.com>
2024-07-23 12:13:11 +0100

    updated readme file

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-
-
 app.get('/', (req, res) => {
-    res.status(400).send({'message': 'Hello World!'})
+    res.status(200).send({'message': 'Hello World!'})
     // res.status(Status code:200).send("Hello world!")
 })
 app.listen(port, () => {
