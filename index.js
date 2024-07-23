6d4f1c6466d3c6425893ca23e20cb9a0b8b4e05e
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-07-23 12:01:50 +0100

    add status method with message

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.listen(port, (req, res) => {
-    console.log(`server started at port: ${port}`)
-})
+
 
 app.get('/', (req, res) => {
-    res.send("Hello world!")
+    res.status(400).send({'message': 'Hello World!'})
+    // res.status(Status code:200).send("Hello world!")
+})
+app.listen(port, () => {
+    console.log(`server started at port: ${port}`)
 })
