1c7df3b4d4ba0f931d4706da4de78a99b8b62085
peposlawa <peposlawa@gmail.com>
2024-07-24 14:04:21 +0100

    epic 2 story 1 completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import helmet from "helmet";
-import dataJson from "./data.json" with { type: "json" } ;
+import dataJson from "./data.json" with { type: "json" };
 
 
 
@@ -9,23 +9,26 @@ app.use(helmet());
 const port = 3000;
 
 app.get('/', (req, res) => {
-try{
-res.status(200).send(dataJson)
+  try {
+    res.status(200).send({
+      success: true,
+      payload: dataJson,
+    });
 
-}catch(error){
+  } catch (error) {
     if (error.message.includes('No valid data ')) {
-        return res.status(404).send({
-          success: false,
-          payload: error.message,
-        });
-      } else {
-        return res.status(500).send({
-          success: false,
-          payload: 'Internal Server Error',
-        });
-      }
-}
+      return res.status(404).send({
+        success: false,
+        payload: error.message,
+      });
+    } else {
+      return res.status(500).send({
+        success: false,
+        payload: 'Internal Server Error',
+      });
+    }
+  }
 })
 app.listen(port, () => {
-    console.log(`server started at port: ${port}`)
+  console.log(`server started at port: ${port}`)
 })
