28f5e770803ba66c56e1ecc1d107002444a10489
peposlawa <peposlawa@gmail.com>
2024-07-24 16:53:26 +0100

    app.post attempt 1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"; // import express
 import helmet from "helmet"; // import helmet for security
 import dataJson from "./data.json" with { type: "json" }; // import the data from the json file
+import { v4 as uuidv4 } from 'uuid'; // import uuid to generate unique ids
 
 const app = express(); // create express app
 app.use(helmet()); // add some security headers to the responses
@@ -8,66 +9,52 @@ const port = 3000; // we will use this port
 app.use(express.json()); // we will use json in the body of the requests
 
 // get all the users
-app.get('/', (req, res) => {
-  try {
+app.get('/activities', (req, res) => {
     return res.status(200).send({
-      error: true,
+      error: false,
       data: dataJson, // return the data from the json file
     });
-  } catch (error) {
-    if (error.message.includes('Unfortunately no valid data')) {
-      return res.status(404).send({
-        error: false,
-        data: error.message,
-      });
-    } else {
-      return res.status(500).send({
-        error: false,
-        data: 'Internal Server Error',
-      });
-    }
-  }
 })
 
 // get the user by the user_id
 app.get('/:user_id', (req, res) => {
-  try {
     const userId = req.params.user_id;
     const user = dataJson.find(user => user.user_id === userId);
-    return res.status(200).send({
-      error: true,
-      data: user,
-    });
-  } catch (error) {
-    if (error.message.includes('Nope, no user with that ID')) {
+    if (!user) {
       return res.status(404).send({
-        error: false,
-        data: error.message,
-      });
-    } else {
-      return res.status(500).send({
-        error: false,
-        data: 'Internal Server Error',
+        error: true,
+        data: 'User not found',
       });
     }
-  }
+    return res.status(200).send({
+      error: false,
+      data: user,
+    });
 })
 
-// get the activities by the user_id
+// add a new activity
 app.post('/activities', (req, res) => {
-  try {
-    const newActivity = req.body;
-    dataJson.push(newActivity);
+    const newActivity = req.body.newActivity;
+    
+    if (!newActivity) {
+      return res.status(400).send({
+        error: true,
+        data: 'Please provide the activity',
+      });
+    }
+    
+    const activity = {
+      ...newActivity,
+      id: uuidv4(),
+      activity_submitted: Date.now(),
+    }
+
+    dataJson.push(activity);
+
     return res.status(200).send({
-      error: true,
+      error: false,
       data: newActivity,
     });
-  } catch (error) {
-    return res.status(500).send({
-      error: false,
-      data: 'Internal Server Error',
-    });
-  }
 });
 
 // start the server
