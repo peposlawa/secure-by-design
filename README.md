5a085bc4d6983120455f554da81f0275d7440f25
peposlawa <peposlawa@gmail.com>
2024-07-24 15:41:41 +0100

    added comments, changed success / payload to  error / data

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ This application is using Node.js with Express API framework.
 Here are the steps for how to install and run the APP:
 
 1. Open terminal and write npm install - this will install all dependencies;
-2. Write npm start to launch the server;
+2. Write npm run dev to launch the server;
 3. Got to postman and use this url http://localhost:3000/ to test the API's response and the status code (should display "200").
--- index.js
+++ index.js
@@ -1,82 +1,76 @@
-import express from "express";
-import helmet from "helmet";
-import dataJson from "./data.json" with { type: "json" };
+import express from "express"; // import express
+import helmet from "helmet"; // import helmet for security
+import dataJson from "./data.json" with { type: "json" }; // import the data from the json file
 
+const app = express(); // create express app
+app.use(helmet()); // add some security headers to the responses
+const port = 3000; // we will use this port
+app.use(express.json()); // we will use json in the body of the requests
 
-
-const app = express();
-app.use(helmet());
-const port = 3000;
-
+// get all the users
 app.get('/', (req, res) => {
   try {
     return res.status(200).send({
-      success: true,
-      payload: dataJson,
+      error: true,
+      data: dataJson, // return the data from the json file
     });
-
   } catch (error) {
-    if (error.message.includes('No valid data ')) {
+    if (error.message.includes('Unfortunately no valid data')) {
       return res.status(404).send({
-        success: false,
-        payload: error.message,
+        error: false,
+        data: error.message,
       });
     } else {
       return res.status(500).send({
-        success: false,
-        payload: 'Internal Server Error',
+        error: false,
+        data: 'Internal Server Error',
       });
     }
   }
 })
+
+// get the user by the user_id
 app.get('/:user_id', (req, res) => {
   try {
-     const userId = req.params.user_id;
+    const userId = req.params.user_id;
     const user = dataJson.find(user => user.user_id === userId);
-
     return res.status(200).send({
-      success: true,
-      payload: user,
+      error: true,
+      data: user,
     });
-
   } catch (error) {
-    if (error.message.includes('No user with that ID')) {
+    if (error.message.includes('Nope, no user with that ID')) {
       return res.status(404).send({
-        success: false,
-        payload: error.message,
+        error: false,
+        data: error.message,
       });
     } else {
       return res.status(500).send({
-        success: false,
-        payload: 'Internal Server Error',
+        error: false,
+        data: 'Internal Server Error',
       });
     }
   }
 })
 
-app.post('/activities', async (req, res) => {
+// get the activities by the user_id
+app.post('/activities', (req, res) => {
   try {
-    const pushActivity = await req.body.dataJson;
+    const newActivity = req.body;
+    dataJson.push(newActivity);
     return res.status(200).send({
-      success: true,
-      payload: pushActivity,
-    })
+      error: true,
+      data: newActivity,
+    });
   } catch (error) {
-    if (error.message.includes('No valid data ')) {
-      return res.status(404).send({
-        success: false,
-        payload: error.message,
-      });
-    } else {
-      return res.status(500).send({
-        success: false,
-        payload: 'Internal Server Error',
-      });
-    }
+    return res.status(500).send({
+      error: false,
+      data: 'Internal Server Error',
+    });
   }
 });
 
-
+// start the server
 app.listen(port, () => {
-  console.log(`server started at port: ${port}`)
+  console.log(`hello! server started at port: ${port}`)
 })
