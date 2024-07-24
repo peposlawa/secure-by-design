81460e7fa4731ce5576c625e29d7fb355bc9efe6
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-07-24 12:31:54 +0100

    get data from json file

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 import express from "express";
 import helmet from "helmet";
+import dataJson from "./data.json" with { type: "json" } ;
+
 
 const app = express();
 app.use(helmet());
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).send({ 'message': 'Hello World!' })
-    // res.status(Status code:200).send("Hello world!")
+app.get('/', async(req, res) => {
+try{
+const response = await dataJson;
+    
+res.status(200).send(response)
+
+}catch(error){
+    if (error.message.includes('No valid data ')) {
+        return res.status(404).send({
+          success: false,
+          payload: error.message,
+        });
+      } else {
+        return res.status(500).send({
+          success: false,
+          payload: 'Internal Server Error',
+        });
+      }
+}
 })
 app.listen(port, () => {
     console.log(`server started at port: ${port}`)
