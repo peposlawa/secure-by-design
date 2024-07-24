57ba6d0cf5176db1191fad657d61333a98332e81
kieranp-p6 <kiip96@icloud.com>
2024-07-24 14:26:34 +0100

    jfhskss

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   try {
-    res.status(200).send({
+    return res.status(200).send({
       success: true,
       payload: dataJson,
     });
@@ -29,6 +29,52 @@ app.get('/', (req, res) => {
     }
   }
 })
+app.get('/:user_id', (req, res) => {
+  try {
+    const userId = req.params.user_id;
+    return res.status(200).send({
+      success: true,
+      payload: user,
+    });
+
+  } catch (error) {
+    if (error.message.includes('No user with that ID')) {
+      return res.status(404).send({
+        success: false,
+        payload: error.message,
+      });
+    } else {
+      return res.status(500).send({
+        success: false,
+        payload: 'Internal Server Error',
+      });
+    }
+  }
+})
+
+app.post('/activities', async (req, res) => {
+  try {
+    const pushActivity = await req.body.dataJson;
+    return res.status(200).send({
+      success: true,
+      payload: pushActivity,
+    })
+  } catch (error) {
+    if (error.message.includes('No valid data ')) {
+      return res.status(404).send({
+        success: false,
+        payload: error.message,
+      });
+    } else {
+      return res.status(500).send({
+        success: false,
+        payload: 'Internal Server Error',
+      });
+    }
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`server started at port: ${port}`)
 })
